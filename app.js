3d1e6344f63f837ea819e46efa7d02ff57a9ff0b
Mali Pravin <malipravin5545@gmail.com>
2026-02-17 10:04:18 +0530

    new commit

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-console.log("hello")
-console.log("hello")
-console.log("hello mr morning")
-console.log("no way")
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.write("Hello Node.js Project");
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
